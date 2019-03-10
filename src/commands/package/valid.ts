63e257b725ae1acc3d711c12745f92df747fafea
Azlam Abdulsalam <azlam.salam@outlook.com>
2019-03-10 17:58:11 +1100

    Updated to v45 metadata json export from SF

--- src/commands/package/valid.ts
+++ src/commands/package/valid.ts
@@ -1,11 +1,15 @@
-import {core, flags, SfdxCommand} from '@salesforce/command';
-import {AnyJson, toJsonMap} from '@salesforce/ts-types';
-import {JsonArray,JsonMap} from '@salesforce/ts-types';
-import { SfdxProject } from '@salesforce/core';
+import { core, flags, SfdxCommand } from '@salesforce/command';
+import { AnyJson, toJsonMap } from '@salesforce/ts-types';
+import { JsonArray, JsonMap } from '@salesforce/ts-types';
+import { SfdxProject, SfdxError } from '@salesforce/core';
 import xml2js = require('xml2js');
 import util = require('util');
 import fs = require('fs-extra');
-import coverageJSON from '../../coverage.json';
+import coverageJSON from '../../metadata.json';
+import rimraf = require('rimraf');
+import { integer } from '@oclif/parser/lib/flags';
+import { Analytics } from 'jsforce';
+import { ClientRequest } from 'http';
 
 
 const spawn = require('child-process-promise').spawn;
@@ -23,27 +27,27 @@ export default class Valid extends SfdxCommand {
   public static description = messages.getMessage('commandDescription');
 
   public static examples = [
-  `$ sfdx hello:org --targetusername myOrg@example.com --targetdevhubusername devhub@org.com
+    `$ sfdx hello:org --targetusername myOrg@example.com --targetdevhubusername devhub@org.com
   Hello world! This is org: MyOrg and I will be around until Tue Mar 20 2018!
   My hub org id is: 00Dxx000000001234
   `,
-  `$ sfdx hello:org --name myname --targetusername myOrg@example.com
+    `$ sfdx hello:org --name myname --targetusername myOrg@example.com
   Hello myname! This is org: MyOrg and I will be around until Tue Mar 20 2018!
   `
   ];
 
-  public static args = [{name: 'file'}];
+  public static args = [{ name: 'file' }];
 
   protected static flagsConfig = {
-    package: flags.string({required:false, char: 'n', description: messages.getMessage('packageFlagDescription')}),
-    
+    package: flags.string({ required: false, char: 'n', description: messages.getMessage('packageFlagDescription') }),
+
   };
 
   // Comment this out if your command does not require an org username
   //protected static requiresUsername = true;
 
   // Comment this out if your command does not support a hub org username
- // protected static supportsDevhubUsername = true;
+  // protected static supportsDevhubUsername = true;
 
   // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
   protected static requiresProject = true;
@@ -54,50 +58,76 @@ export default class Valid extends SfdxCommand {
     const project = await SfdxProject.resolve();
 
     const projectJson = await project.retrieveSfdxProjectJson();
-    
+
     let packageToBeScanned = this.flags.package;
-    this.ux.log("package:"+packageToBeScanned);
-  
+    this.ux.log("package:" + packageToBeScanned);
+
 
     const packageDirectories = projectJson.get('packageDirectories') as JsonArray || [];
-    this.ux.logJson(packageDirectories);
+    // this.ux.logJson(packageDirectories);
+    const result_store = [];
 
+    if (packageToBeScanned != undefined) {
+      for (const sf_package of (packageDirectories as JsonArray)) {
 
-    for (const sf_package of (packageDirectories as JsonArray)) {
-        
-      if(packageToBeScanned!=undefined && packageToBeScanned === sf_package['package'])
-      {
-        this.ux.log("found");
-        this.ux.logJson(sf_package.valueOf());
-        await this.validate(sf_package);
-        break;
+        if (packageToBeScanned != undefined && packageToBeScanned === sf_package['package']) {
+          this.ux.log("Package to be analyzed located");
+          //this.ux.logJson(sf_package.valueOf());
+          let result;
+          try {
+            result = await this.validate(sf_package);
+            break;
+          }
+          catch (e) {
+            this.clearDirectory();
+            this.ux.log("Error Occured Unable to analyze");
+          }
+        }
+
       }
-      
+    }
+    else {
+
+      this.ux.log("All packaging directories are  being analyzed");
+
+      for (const sf_package of (packageDirectories as JsonArray)) {
+        if (sf_package['package'] != undefined) {
+          this.ux.log(`Now analyzing ${sf_package['package']}`);
+          //this.ux.logJson(sf_package.valueOf());
+          let result;
+          try {
+            result = await this.validate(sf_package);
+          }
+          catch (e) {
+            this.clearDirectory();
+            this.ux.log("Error Occured Unable to analyze");
+          }
+          result_store.push(result);
+
+        }
+      }
     }
 
-    //Iterate through packages
-    //Find the default package
-    //Figure all the metadata and write it to a json
-    //Compare json with metadata json
-    //Output what is crap 
-    
+    result_store.forEach(element => {
 
-    
-    return { };
+      if (element == 1)
+        throw new SfdxError("Analysis Failed, Unsupported metadata present")
+
+    });
+
+
+    return { message: 'Analyzing succesfully completed' };
   }
 
-  public async  validate(packageToBeScanned:AnyJson)
-  {
-    this.ux.log("found2");
-    this.ux.logJson(packageToBeScanned.valueOf());
-   
+  public async  validate(packageToBeScanned: AnyJson) {
+
     // Split arguments to use spawn
     const args = [];
     args.push('force:source:convert');
 
     // outputdir
     args.push('-d');
-    args.push('mdapi');
+    args.push('temp_sfpowerkit/mdapi');
 
     // package name
     args.push('-n');
@@ -109,53 +139,57 @@ export default class Valid extends SfdxCommand {
 
     // INSTALL PACKAGE
     this.ux.log(`Converting package ${packageToBeScanned['package']}`);
-    
-    
+
+
     var startTime = (new Date()).valueOf();
     await spawn('sfdx', args, { stdio: 'inherit' });
 
-    let targetFilename = 'mdapi/package.xml';
+    let targetFilename = 'temp_sfpowerkit/mdapi/package.xml';
 
     if (fs.existsSync(targetFilename)) {
       const parser = new xml2js.Parser({ explicitArray: false });
       const parseString = util.promisify(parser.parseString);
-      const existing = await parseString(fs.readFileSync(targetFilename));     
+      const existing = await parseString(fs.readFileSync(targetFilename));
 
-      const badtypes =[];
-   
+      const unsupportedTypes = [];
+      const supportedTypes = [];
+
       for (const types of (existing.Package.types as JsonArray)) {
 
-        coverageJSON.forEach(element => {
-          if(types['name']===element.Metadata)
-          {
-           if(element.isUnlockedPackagingSupported)
-             this.ux.log(`${element.Metadata} is Supported`)
-           else
-            badtypes.push(element.Metadata);
-          }
-        });
-        
+
+
+        if (coverageJSON.types[types['name']] != undefined)
+          if (coverageJSON.types[types['name']].channels.unlockedPackagingWithoutNamespace)
+            supportedTypes.push(`${types['name']}`);
+          else
+            unsupportedTypes.push(`${types['name']}`);
 
       }
+     
+      if(supportedTypes.length>0)
+      {
+      this.ux.log(`Elements supported included in your package ${packageToBeScanned['package']} are`);
+      this.ux.logJson(supportedTypes);
+      }
 
-    if(badtypes.length>0)
-    {
-      this.ux.log("Elements not supported are ");
-      this.ux.logJson(badtypes);
-      throw new Error("Unsupported metadata found in packaging folder");
-    }
-  
-
+      if (unsupportedTypes.length > 0) {
+        this.ux.log("Elements not supported are ");
+        this.ux.logJson(unsupportedTypes);
+        this.clearDirectory();
+        return 1;
+      }
     } else {
-      throw new Error(`Not found: ${targetFilename}`);
+      return 2;
     }
-  
 
-    return { message:'Success, All metadata are packageable!'};
+
+
+    this.clearDirectory();
+    return 0;
 
   }
 
-  
-
-
+  public async clearDirectory() {
+    rimraf.sync('temp_sfpowerkit');
+  }
 }
