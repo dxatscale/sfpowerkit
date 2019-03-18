b0d5a1242d65498e060953228356527f368e8d3c
Azlam Abdulsalam <azlam.salam@outlook.com>
2019-03-18 23:52:28 +1100

    Updated Repo

--- src/commands/sfpowerkit/org/connectedapp/create.ts
+++ src/commands/sfpowerkit/org/connectedapp/create.ts
@@ -44,7 +44,7 @@ export default class Create extends SfdxCommand {
   public static description = messages.getMessage('commandDescription');
 
   public static examples = [
-    `$ sfdx  sfpowerkit:org:connectedapp:create -u myOrg@example.com -n AzurePipelines -c id_rsa -e azlam.abdulsalam@accentue.com
+    `$ sfdx  sfpowerkit:org:connectedapp:create -u myOrg@example.com -n AzurePipelines -c id_rsa -e azlam.salamm@invalid.com
   Created Connected App AzurePipelines in Target Org
   `
   ];
@@ -146,7 +146,7 @@ export default class Create extends SfdxCommand {
 
    rimraf.sync('temp_sfpowerkit');
 
-    return {}
+    return  {'connectedapp.consumerkey': this.connectedapp_consumerKey };
   }
 
   public createConsumerKey() {
