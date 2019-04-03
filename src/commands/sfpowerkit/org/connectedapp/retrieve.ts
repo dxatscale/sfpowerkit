9c6ef30d83c8339af293801d2e872d4fbced0e3e
Azlam Abdulsalam <azlam.salam@outlook.com>
2019-04-04 00:39:28 +1100

    Added retiree

--- src/commands/sfpowerkit/org/connectedapp/retrieve.ts
+++ src/commands/sfpowerkit/org/connectedapp/retrieve.ts
@@ -1,13 +1,18 @@
 
 import { AnyJson } from '@salesforce/ts-types';
-
+import fs from 'fs-extra';
 import { core, flags, SfdxCommand } from '@salesforce/command';
 import rimraf = require('rimraf');
 import { RetrieveResultLocator, AsyncResult, Callback, AsyncResultLocator, Connection, RetrieveResult } from 'jsforce';
 import { AsyncResource } from 'async_hooks';
 import { SfdxError } from '@salesforce/core';
+import xml2js = require('xml2js');
+import util = require('util');
 // tslint:disable-next-line:ordered-imports
 var jsforce = require('jsforce');
+var path = require('path')
+var unzipper = require('unzipper')
+
 
 
 
@@ -62,95 +67,119 @@ export default class Retrieve extends SfdxCommand {
 
   public async run(): Promise<AnyJson> {
 
-    rimraf.sync('temp_sfpowerkit');
+    // rimraf.sync('temp_sfpowerkit');
 
-    this.loginUrl = `https://test.salesforce.com`
-
-
-
-
-    let conn = new Connection({
-      // you can change loginUrl to connect to sandbox or prerelease env.
-      loginUrl: 'https://test.salesforce.com'
-
-    });
-
-
-
-    await conn.login(this.flags.username, this.flags.password, function (err, userInfo) {
-      if (err) { return console.error(err); }
-      // Now you can get the access token and instance URL information.
-      // Save them to establish connection next time.
-      console.log(conn.accessToken);
-      console.log(conn.instanceUrl);
-      // logged in user property
-      console.log("User ID: " + userInfo.id);
-      console.log("Org ID: " + userInfo.organizationId);
-      // ...
-    });
+    // this.loginUrl = `https://test.salesforce.com`
 
 
 
 
-    let retrieveRequest = {
-      apiVersion: 45.0
-    };
+    // let conn = new Connection({
+    //   // you can change loginUrl to connect to sandbox or prerelease env.
+    //   loginUrl: 'https://test.salesforce.com'
 
-    retrieveRequest['singlePackage'] = true;
-    retrieveRequest['unpackaged'] = { types: { name: 'ConnectedApp', members: this.flags.name } };
-
-    this.ux.logJson(retrieveRequest);
+    // });
 
 
-    conn.metadata.pollTimeout = 60;
 
-    let retrievedId;
-    let retrievedState;
-
-    await conn.metadata.retrieve(retrieveRequest, function (error, result: AsyncResult) {
-
-      if (error) { return console.error(error); }
-
-      retrievedId = result.id;
-      retrievedState = result.state;
-      console.log(result.id);
-      console.log(result.state);
-    });
-
-
-    let metadata_result;
-
-    while (true) {
-
-      await conn.metadata.checkRetrieveStatus(retrievedId, function (error, result: RetrieveResult) {
-        if (error) { return console.error(error); }
-        metadata_result = result
-      });
-
-
-      if (metadata_result.status == 'Pending') {
-        console.log("Polling Metadata");
-        this.ux.logJson(metadata_result);
-        await (this.delay(2000));
-      }
-      else {
-        this.ux.logJson(metadata_result);
-        break;
-      }
-    }
-
-
-   //Unzip Zipfile
-   //Convert XML To JSON
-   //JSON Output to Screen
+    // await conn.login(this.flags.username, this.flags.password, function (err, userInfo) {
+    //   if (err) { return console.error(err); }
+    //   // Now you can get the access token and instance URL information.
+    //   // Save them to establish connection next time.
+    //   console.log("Successfully authenticated")
+    //   // logged in user propertythis.ux.
+    //   console.log("User ID: " + userInfo.id);
+    //   console.log("Org ID: " + userInfo.organizationId);
+    //   // ...
+    // });
 
 
 
 
+    // let retrieveRequest = {
+    //   apiVersion: 45.0
+    // };
+
+    // retrieveRequest['singlePackage'] = true;
+    // retrieveRequest['unpackaged'] = { types: { name: 'ConnectedApp', members: this.flags.name } };
+
+    // this.ux.logJson(retrieveRequest);
 
 
+    // conn.metadata.pollTimeout = 60;
 
-    return { 'connectedapp.consumerkey': this.connectedapp_consumerKey };
+    // let retrievedId;
+
+    // await conn.metadata.retrieve(retrieveRequest, function (error, result: AsyncResult) {
+
+    //   if (error) { return console.error(error); }
+    //   retrievedId = result.id;
+    //   console.log(retrievedId);
+    // });
+
+
+    // let metadata_result;
+    // let count = 0
+
+    // while (true) {
+
+    //   count++;
+    //   await conn.metadata.checkRetrieveStatus(retrievedId, function (error, result: RetrieveResult) {
+    //     if (error) { return console.error(error); }
+    //     metadata_result = result
+    //   });
+
+
+    //   if (metadata_result.status == 'Pending') {
+    //     this.ux.log(`Polling ${count}`)
+    //     this.ux.logJson(metadata_result);
+    //     await (this.delay(5000));
+    //   }
+    //   else {
+
+    //     this.ux.logJson(metadata_result);
+    //     break;
+    //   }
+    // }
+
+    // if (!metadata_result.zipFile)
+    //   throw new SfdxError("Unable to find the requested connectedapp");
+
+
+    var zipFileName = "temp_sfpowerkit/unpackaged.zip";
+
+
+    // fs.mkdirSync('temp_sfpowerkit');
+    // fs.writeFileSync(zipFileName, metadata_result.zipFile, { encoding: 'base64' });
+
+    fs.createReadStream(zipFileName)
+    .pipe(unzipper.Extract({ path: 'temp_sfpowerkit' }));
+
+    
+
+    let resultFile = `temp_sfpowerkit/connectedApps/${this.flags.name}.connectedApp`;
+    this.ux.log(`Checking for file ${resultFile}`);
+
+    this.ux.log(path.resolve(resultFile));
+    let retrieved_connectedapp;
+
+    //if (fs.existsSync(path.resolve(resultFile))) {
+      const parser = new xml2js.Parser({ explicitArray: false });
+      const parseString = util.promisify(parser.parseString);
+      retrieved_connectedapp = await parseString(fs.readFileSync(path.resolve(resultFile)));
+      
+      this.ux.log(`Retrieved ConnectedApp Succesfully  with Consumer Key : ${retrieved_connectedapp.oauthConfig.consumerKey}`);
+    // }
+    // else {
+    //   this.ux.log("Here");
+    //   throw new SfdxError("Unable to process")
+
+    // }
+
+
+    return { 'connectedapp': retrieved_connectedapp };
+
+
   }
 
 
@@ -158,6 +187,8 @@ export default class Retrieve extends SfdxCommand {
   public async  delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
+
+
 
 }
 
