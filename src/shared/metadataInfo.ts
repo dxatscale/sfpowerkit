10aa5a1c747ec6ca6cbb0c46661d566dfb167863
Genoud Magloire DOUANLA DJATIO <g.douanla.djatio@accenture.com>
2019-07-31 00:13:08 +0400

    Move metadatainfo.json from src to resources folder

--- src/shared/metadataInfo.ts
+++ src/shared/metadataInfo.ts
@@ -1,7 +1,6 @@
-import metadataInfoJSON from "../metadatainfo.json";
 import * as _ from "lodash";
-import { SfPowerKit } from "./sfpowerkit";
-import * as path from 'path'
+import * as path from 'path';
+import * as fs from 'fs';
 
 export const SOURCE_EXTENSION_REGEX = /\.[a-zA-Z]+-meta\.xml/;
 const SPLITED_TYPES = {
@@ -94,6 +93,9 @@ export interface MetadataInfo {
 export class MetadataInfoUtils {
   static loadMetadataInfo(): MetadataInfo {
     let metadataInfo: MetadataInfo = {};
+    let resourcePAth= path.join(__dirname,'..','..', 'resources', 'metadatainfo.json')
+    const fileData = fs.readFileSync(resourcePAth, 'utf8');
+    let metadataInfoJSON= JSON.parse(fileData);
     metadataInfoJSON.metadataObjects.forEach(metadata => {
       let metadataDescribe = metadata as MetadataDescribe;
       if(_.isNil(metadata.suffix)){
