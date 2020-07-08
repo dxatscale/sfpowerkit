acafa94bbeb4c00cd1af6582f0f3e860e1c3e496
mani <manivasaga.murugesan@accenture.com>
2020-07-08 20:41:16 +1000

    customfield markdown generate

--- src/commands/sfpowerkit/source/datamodel/report.ts
+++ src/commands/sfpowerkit/source/datamodel/report.ts
@@ -27,7 +27,8 @@ export default class Report extends SfdxCommand {
     `$ sfdx sfpowerkit:source:datamodel:report`,
     `$ sfdx sfpowerkit:source:datamodel:report -p force-app/main/default/objects -d result`,
     `$ sfdx sfpowerkit:source:datamodel:report -p force-app/main/default/objects -t CustomField,RecordType`,
-    `$ sfdx sfpowerkit:source:datamodel:report -p force-app/main/default/objects -t CustomField,RecordType -f csv`
+    `$ sfdx sfpowerkit:source:datamodel:report -p force-app/main/default/objects -t CustomField,RecordType -f csv`,
+    `$ sfdx sfpowerkit:source:datamodel:report -f md -d docs`
   ];
 
   protected static flagsConfig = {
@@ -58,7 +59,7 @@ export default class Report extends SfdxCommand {
       required: false,
       char: "f",
       description: messages.getMessage("formatDescription"),
-      options: ["json", "csv"],
+      options: ["json", "csv", "md"],
       default: "json"
     }),
     loglevel: flags.enum({
@@ -121,6 +122,7 @@ export default class Report extends SfdxCommand {
       metadataFiles = metadataFiles.concat(FileUtils.getAllFilesSync(dir));
     }
 
+    let counter = 0;
     let result = [];
     for (let metadataFile of metadataFiles) {
       for (let type of this.flags.filtertype) {
@@ -134,29 +136,19 @@ export default class Report extends SfdxCommand {
             metadataDescribe.sourceExtension
           );
 
-          let parentName = "";
-          if (metadataDescribe.isChildComponent) {
-            let fileParts = metadataFile.split(path.sep);
-            parentName = fileParts[fileParts.length - 3];
-          }
+          let fileParts = metadataFile.split(path.sep);
 
           let metadataJson = await xmlUtil.xmlToJSON(metadataFile);
+          delete metadataJson[type]["$"];
 
           let component = {
-            metadatype: type,
             name: name,
-            objectName: parentName,
-            description: metadataJson[type].description
-              ? metadataJson[type].description
+            metadatype: type,
+            objectName: metadataDescribe.isChildComponent
+              ? fileParts[fileParts.length - 3]
               : "",
-            fieldType: metadataJson[type].type ? metadataJson[type].type : "",
-            status:
-              metadataJson[type].active != null
-                ? metadataJson[type].active
-                  ? "Active"
-                  : "Inactive"
-                : "",
-            path: metadataFile.split("\\").join("/")
+            sourcePath: metadataFile.split("\\").join("/"),
+            metadataJson: metadataJson[type]
           };
 
           result.push(component);
@@ -169,8 +161,10 @@ export default class Report extends SfdxCommand {
 
       if (this.flags.json || this.flags.format === "json") {
         await this.generateJsonOutput(result, this.flags.outputdir);
-      } else {
+      } else if (this.flags.format === "csv") {
         await this.generateCSVOutput(result, this.flags.outputdir);
+      } else {
+        this.generateMdOutput(result, this.flags.outputdir);
       }
     } else {
       SFPowerkit.log(
@@ -178,7 +172,7 @@ export default class Report extends SfdxCommand {
         LoggerLevel.INFO
       );
     }
-    return JSON.stringify(result);
+    return result;
   }
   public async generateCSVOutput(result: any[], outputDir: string) {
     let outputcsvPath = `${outputDir}/output.csv`;
@@ -191,7 +185,14 @@ export default class Report extends SfdxCommand {
       "Metadata type,Name,ObjectName,fieldType,status(recordtypes/validation rules),source Path" +
       newLine;
     result.forEach(element => {
-      output = `${output}${element.metadatype},${element.name},${element.objectName},${element.fieldType},${element.status},${element.path}${newLine}`;
+      let fieldType =
+        element.metadatype === "CustomField" ? element.metadataJson.type : "";
+      let status =
+        element.metadatype === "RecordType" ||
+        element.metadatype === "ValidationRule"
+          ? element.metadataJson.active
+          : "";
+      output = `${output}${element.metadatype},${element.name},${element.objectName},${fieldType},${status},${element.sourcePath}${newLine}`;
     });
     fs.writeFileSync(outputcsvPath, output);
     SFPowerkit.log(
@@ -210,5 +211,217 @@ export default class Report extends SfdxCommand {
       `Output ${outputDir}/output.json is generated successfully`,
       LoggerLevel.INFO
     );
+  }
+
+  private generateMdOutput(result: any[], outputDir: string) {
+    for (let item of result) {
+      let filepath = outputDir + item.sourcePath.split("objects")[1] + ".md";
+      filepath = filepath.split("/").join("\\");
+      let dir = path.parse(filepath).dir;
+      if (!fs.existsSync(dir)) {
+        FileUtils.mkDirByPathSync(dir);
+      }
+      this.generateMdFromType(filepath, item);
+    }
+    SFPowerkit.log(
+      `Output markdown files generated in ${outputDir} successfully`,
+      LoggerLevel.INFO
+    );
+  }
+  private generateMdFromType(filepath: string, file: any) {
+    if (file.metadatype === "CustomField") {
+      this.generateMdforCustomField(filepath, file.metadataJson);
+    }
+  }
+  private generateMdforCustomField(filepath: string, metadataJson: any) {
+    let codeblock = "\n````\n";
+    let field = `## Name : ${metadataJson.fullName} \n---\n`;
+    if (metadataJson.label) {
+      field = `${field}**Label** : ${metadataJson.label}\n`;
+    }
+    if (metadataJson.type) {
+      field = `${field}**Type** : ${metadataJson.type}\n`;
+    }
+    if (metadataJson.length) {
+      field = `${field}**Length** : ${metadataJson.length}\n`;
+    }
+    if (metadataJson.precision) {
+      field = `${field}**Decimal Precision** : ${metadataJson.precision}\n`;
+    }
+    if (metadataJson.visibleLines) {
+      field = `${field}**Visible Lines** : ${metadataJson.visibleLines}\n`;
+    }
+    if (metadataJson.required) {
+      field = `${field}**Required** : ${metadataJson.required}\n`;
+    }
+    if (metadataJson.unique) {
+      field = `${field}**Unique** : ${metadataJson.unique}\n`;
+    }
+    if (metadataJson.description) {
+      field = `${field}**Description** : ${metadataJson.description}\n`;
+    }
+    if (metadataJson.defaultValue) {
+      field = `${field}**Default Value** : ${metadataJson.defaultValue}\n`;
+    }
+    if (metadataJson.deprecated) {
+      field = `${field}**Deprecated** : ${metadataJson.deprecated}\n`;
+    }
+    if (metadataJson.caseSensitive) {
+      field = `${field}**Case Sensitive** : ${metadataJson.caseSensitive}\n`;
+    }
+    if (metadataJson.complianceGroup) {
+      field = `${field}**Compliance Group** : ${metadataJson.complianceGroup}\n`;
+    }
+    if (metadataJson.deleteConstraint) {
+      field = `${field}**Delete Constraint** : ${metadataJson.deleteConstraint}\n`;
+    }
+    if (metadataJson.displayFormat) {
+      field = `${field}**Display Format** : ${metadataJson.displayFormat}\n`;
+    }
+    if (metadataJson.startingNumber) {
+      field = `${field}**Starting Number** : ${metadataJson.startingNumber}\n`;
+    }
+    if (metadataJson.encrypted) {
+      field = `${field}**Encrypted** : ${metadataJson.encrypted}\n`;
+    }
+    if (metadataJson.encryptionScheme) {
+      field = `${field}**Encryption Scheme** : ${metadataJson.encryptionScheme}\n`;
+    }
+    if (metadataJson.externalDeveloperName) {
+      field = `${field}**External Developer Name** : ${metadataJson.externalDeveloperName}\n`;
+    }
+    if (metadataJson.externalId) {
+      field = `${field}**ExternalId ?** : ${metadataJson.externalId}\n`;
+    }
+    if (metadataJson.fieldManageability) {
+      field = `${field}**FieldManageability** : ${metadataJson.fieldManageability}\n`;
+    }
+    if (metadataJson.formula) {
+      field = `${field}**Formula** : ${codeblock}${metadataJson.formula}${codeblock}`;
+    }
+    if (metadataJson.formulaTreatBlankAs) {
+      field = `${field}**Formula Treat Blank As** : ${metadataJson.formulaTreatBlankAs}\n`;
+    }
+    if (metadataJson.inlineHelpText) {
+      field = `${field}**Help Text** : ${metadataJson.inlineHelpText}\n`;
+    }
+    if (metadataJson.isAIPredictionField) {
+      field = `${field}**AI Prediction Field ?** : ${metadataJson.isAIPredictionField}\n`;
+    }
+    if (metadataJson.isFilteringDisabled) {
+      field = `${field}**Filtering Disabled ?** : ${metadataJson.isFilteringDisabled}\n`;
+    }
+    if (metadataJson.isNameField) {
+      field = `${field}**Name Field ?** : ${metadataJson.isNameField}\n`;
+    }
+    if (metadataJson.isSortingDisabled) {
+      field = `${field}**Sorting Disabled ?** : ${metadataJson.isSortingDisabled}\n`;
+    }
+    if (metadataJson.maskChar) {
+      field = `${field}**Mask Character** : ${metadataJson.maskChar}\n`;
+    }
+    if (metadataJson.maskType) {
+      field = `${field}**Mask Type** : ${metadataJson.maskType}\n`;
+    }
+    if (metadataJson["metadataRelationship​ControllingField"]) {
+      field = `${field}**Metadata Relationship​ Controlling Field** : ${metadataJson["metadataRelationship​ControllingField"]}\n`;
+    }
+    if (metadataJson.populateExistingRows) {
+      field = `${field}**Populate Existing Rows** : ${metadataJson.populateExistingRows}\n`;
+    }
+    if (metadataJson.referenceTargetField) {
+      field = `${field}**Reference Target Field** : ${metadataJson.referenceTargetField}\n`;
+    }
+    if (metadataJson.referenceTo) {
+      field = `${field}**Reference To** : ${metadataJson.referenceTo}\n`;
+    }
+    if (metadataJson.relationshipLabel) {
+      field = `${field}**Relationship Label** : ${metadataJson.relationshipLabel}\n`;
+    }
+    if (metadataJson.relationshipName) {
+      field = `${field}**Relationship Name** : ${metadataJson.relationshipName}\n`;
+    }
+    if (metadataJson.relationshipOrder) {
+      field = `${field}**Relationship Order** : ${metadataJson.relationshipOrder}\n`;
+    }
+    if (metadataJson.reparentableMasterDetail) {
+      field = `${field}**Reparentable MasterDetail ?** : ${metadataJson.reparentableMasterDetail}\n`;
+    }
+    if (metadataJson.scale) {
+      field = `${field}**Scale** : ${metadataJson.scale}\n`;
+    }
+    if (metadataJson.securityClassification) {
+      field = `${field}**Security Classification** : ${metadataJson.securityClassification}\n`;
+    }
+    if (metadataJson.stripMarkup) {
+      field = `${field}**Strip Markup** : ${metadataJson.stripMarkup}\n`;
+    }
+    if (metadataJson.summarizedField) {
+      field = `${field}**Summarized Field** : ${metadataJson.summarizedField}\n`;
+    }
+    if (metadataJson.summaryForeignKey) {
+      field = `${field}**Summary ForeignKey** : ${metadataJson.summaryForeignKey}\n`;
+    }
+    if (metadataJson.summaryOperation) {
+      field = `${field}**Summary Operation** : ${metadataJson.summaryOperation}\n`;
+    }
+    if (metadataJson.trackFeedHistory) {
+      field = `${field}**Track Feed History** : ${metadataJson.trackFeedHistory}\n`;
+    }
+    if (metadataJson.trackHistory) {
+      field = `${field}**Track History** : ${metadataJson.trackHistory}\n`;
+    }
+    if (metadataJson.trackTrending) {
+      field = `${field}**Track Trending** : ${metadataJson.trackTrending}\n`;
+    }
+    if (metadataJson.writeRequiresMasterRead) {
+      field = `${field}**write Requires MasterRead** : ${metadataJson.writeRequiresMasterRead}\n`;
+    }
+    if (metadataJson.summaryFilterItems) {
+      field = `${field}**Summary Filter Items** : \nField | Operation | Value | ValueField \n--- | --- | --- | ---\n`;
+      let members = metadataJson.summaryFilterItems;
+      if (members.constructor === Array) {
+        members.forEach(element => {
+          field = `${field}${element.field} | ${element.operation} | ${element.value} | ${element.valueField} \n`;
+        });
+      } else {
+        field = `${field}${members.field} | ${members.operation} | ${members.value} | ${members.valueField} \n\n`;
+      }
+    }
+    if (metadataJson.valueSet) {
+      field = `${field}**Picklist valueset** : \n`;
+      field = `${field}Setting | Value \n --- | ---\n`;
+      field = `${field}Restricted Picklist ? | ${metadataJson.valueSet.restricted} \n`;
+      if (metadataJson.valueSet.valueSetName) {
+        field = `${field}Globalvalueset | ${metadataJson.valueSet.valueSetName} \n`;
+      }
+
+      if (metadataJson.valueSet.controllingField) {
+        field = `${field}Controlling Field | ${metadataJson.valueSet.controllingField} \n`;
+      } else if (metadataJson.valueSet.valueSetDefinition) {
+        field = `${field}Sorted | ${metadataJson.valueSet.valueSetDefinition.sorted} \n\n`;
+
+        field = `${field}Label | Api Name | default\n---|---|---\n`;
+        let members = metadataJson.valueSet.valueSetDefinition.value;
+        if (members.constructor === Array) {
+          members.forEach(element => {
+            field = `${field}${element.label} | ${element.fullName} | ${element.default}\n`;
+          });
+          field = `${field}\n`;
+        } else {
+          field = `${field}${members.label} | ${members.fullName} | ${members.default}\n\n`;
+        }
+      }
+      if (metadataJson.valueSet.valueSettings) {
+        if (metadataJson.valueSet.constructor === Array) {
+          //field = `${field}Controlling Field | ${metadataJson.valueSet.controllingField} \n`
+        } else {
+        }
+      }
+    }
+    if (metadataJson.lookupFilter) {
+      field = `${field}**Lookup Filter** : \n`;
+    }
+    fs.writeFileSync(filepath, field);
   }
 }
