c5449c1314c698f656022b9fcae766fcc92790a8
mani <manivasaga.murugesan@accenture.com>
2020-08-09 01:39:28 +1000

    add addtional details to packager version info from devhub

--- src/impl/package/version/packageInfo.ts
+++ src/impl/package/version/packageInfo.ts
@@ -29,4 +29,39 @@ export default class PackageInfo {
     );
     return packageDetails;
   }
+  public async getPackagesDetailsfromDevHub(
+    hubconn: Connection,
+    pkgDetails: PackageDetail[]
+  ): Promise<PackageDetail[]> {
+    let pkgIds: string[] = [];
+    pkgDetails.forEach(pkg => {
+      if (pkg.type === "Unlocked") {
+        pkgIds.push(pkg.packageVersionId);
+      }
+    });
+
+    if (pkgIds.length > 0) {
+      let installedPackagesQuery = `SELECT SubscriberPackageVersionId, HasPassedCodeCoverageCheck,CodeCoverage,ValidationSkipped FROM Package2Version WHERE SubscriberPackageVersionId IN('${pkgIds.join(
+        "','"
+      )}')`;
+      let response = (await hubconn.tooling.query(
+        installedPackagesQuery
+      )) as any;
+      if (response.records && response.records.length > 0) {
+        response.records.forEach(record => {
+          for (let pkg of pkgDetails) {
+            if (pkg.packageVersionId === record.SubscriberPackageVersionId) {
+              pkg.codeCoverageCheckPassed = record.HasPassedCodeCoverageCheck;
+              pkg.CodeCoverage = record.CodeCoverage
+                ? record.CodeCoverage.apexCodeCoveragePercentage
+                : 0;
+              pkg.validationSkipped = record.ValidationSkipped;
+            }
+          }
+        });
+      }
+    }
+
+    return pkgDetails;
+  }
 }
