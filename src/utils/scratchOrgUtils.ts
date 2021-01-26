d802ff12dd56e27ae5792c54acae6bfc69ba6747
mani <manivasaga.murugesan@accenture.com>
2021-01-26 22:16:09 +1100

    fix passwordData

--- src/utils/scratchOrgUtils.ts
+++ src/utils/scratchOrgUtils.ts
@@ -193,7 +193,7 @@ export default class ScratchOrgUtils {
       throw new Error("Unable to setup password to scratch org");
     } else {
       SFPowerkit.log(
-        `Password successfully set for ${result.username} : ${result.password}`,
+        `Password successfully set for ${passwordData.username} : ${passwordData.password}`,
         LoggerLevel.INFO
       );
     }
