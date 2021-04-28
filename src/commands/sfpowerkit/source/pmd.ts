04f9ec3d9a11c186530e9973f6ec03ae4bae9fd8
Rodolphe Blancho <rodolphe.blancho@gmail.com>
2021-04-28 11:59:01 +1000

    update PMD to version 6.34.0. (#508)
    The default PMD version is now 6.34.0.
    The deprecated rules have been removed from
    `pmd-ruleset.xml` and have been replaced by
    the new rules equivalent.

--- src/commands/sfpowerkit/source/pmd.ts
+++ src/commands/sfpowerkit/source/pmd.ts
@@ -60,7 +60,7 @@ export default class Pmd extends SfdxCommand {
     }),
     version: flags.string({
       required: false,
-      default: "6.30.0",
+      default: "6.34.0",
       description: messages.getMessage("versionFlagDescription"),
     }),
     loglevel: flags.enum({
