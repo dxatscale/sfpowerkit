815e520293b5927dbbf1f1bd75f1b271e6139f97
sfpowerscripts <sfpowerscripts@dxscale>
2021-06-18 10:33:29 +1000

    Fix Deepscan issues

--- src/commands/sfpowerkit/package/dependencies/install.ts
+++ src/commands/sfpowerkit/package/dependencies/install.ts
@@ -7,7 +7,6 @@ import { SfdxProject } from "@salesforce/core";
 import { loadSFDX } from "../../../../sfdxnode/GetNodeWrapper";
 import { sfdx } from "../../../..//sfdxnode/parallel";
 import { SFPowerkit, LoggerLevel } from "../../../../sfpowerkit";
-import { isNullOrUndefined } from "util";
 import { get18DigitSalesforceId } from "./../../../../utils/get18DigitSalesforceId";
 let retry = require("async-retry");
 
