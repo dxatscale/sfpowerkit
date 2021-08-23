ae794fdfbdf1fdf92e869bf945bcb0eedcf4b794
Alan Ly <36793262+aly76@users.noreply.github.com>
2021-08-24 07:59:33 +1000

    Update typescript compiler dependency (#552)
    * Update typescript dependency
    
    * * tested and validated the json schema
    * brought back the vaildation functions and added a positive conditional on max allocation.
    
    * Validate max_allocation field
    
    * Clarified that don't need validation to check whether both userpool and pool specified max allocation - removed
    
    Co-authored-by: Henry Lay <henry.lay@amac02che0jmd6r.modem>
    Co-authored-by: Azlam <43767972+azlam-abdulsalam@users.noreply.github.com>

--- src/impl/pool/scratchorg/poolCreateImpl.ts
+++ src/impl/pool/scratchorg/poolCreateImpl.ts
@@ -10,7 +10,7 @@ import FileUtils from "../../../utils/fileutils";
 import * as path from "path";
 import * as rimraf from "rimraf";
 import { SfdxApi } from "../../../sfdxnode/types";
-
+import Ajv from "ajv";
 
 export default class PoolCreateImpl {
   private hubConn: Connection;
@@ -26,7 +26,7 @@ export default class PoolCreateImpl {
   private scriptExecutorWrappedForBottleneck;
   private ipRangeRelaxerWrappedForBottleneck;
 
-  
+
   public constructor(
     private poolconfigFilePath: string,
     private hubOrg: Org,
@@ -44,7 +44,7 @@ export default class PoolCreateImpl {
     this.ipRangeRelaxerWrappedForBottleneck = this.limiter.wrap(
       this.ipRangeRelaxer
     );
-    
+
   }
 
   public async poolScratchOrgs(): Promise<boolean> {
@@ -85,7 +85,7 @@ export default class PoolCreateImpl {
     );
 
     //Temporarily remove validate SO Pool Config
-    // this.validateSoPoolConfig(this.poolConfig)
+    this.validateSoPoolConfig(this.poolConfig)
 
     //Validate Inputs
     if (isNullOrUndefined(this.poolConfig.pool.config_file_path)) {
@@ -734,8 +734,45 @@ export default class PoolCreateImpl {
       return obj;
     }, {});
 
- 
+    private validateSoPoolConfig(
+      soPoolConfig: PoolConfig
+    ): void {
   
+      if (!soPoolConfig.pool.max_allocation && !soPoolConfig.poolUsers) {
+        throw new Error(
+          "Max allocation field is required for tag mode."
+        );
+      }
+
+      let schema = fs.readJSONSync(
+        path.join(
+          __dirname,
+          "..",
+          "..",
+          "..",
+          "..",
+          "resources",
+          "so_pool_config.schema.json"
+        ),
+        {encoding: "UTF-8"}
+      );
+  
+      let validator = new Ajv({allErrors: true}).compile(schema);
+      let validationResult = validator(soPoolConfig);
+      
+
+      if (!validationResult) {
+        let errorMsg: string =
+          `SO Pool Config does not meet schema requirements, ` +
+          `found ${validator.errors.length} validation errors:\n`;
+  
+          validator.errors.forEach((error,errorNum) => {
+            errorMsg += `\n${errorNum+1}: ${error.schemaPath}: ${error.message} ${JSON.stringify(error.params, null, 4)}`;
+            });
+  
+        throw new Error(errorMsg);
+      }
+    }
 }
 
 export interface PoolConfig {
