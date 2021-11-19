713009fea83004ca1a91ad712dca9bddabc4ad70
Henry <henry88lay@gmail.com>
2021-11-19 16:31:08 +1100

    imported SFPKLogger into SFPowerkitCommand to display header

--- src/sfpowerkitCommand.ts
+++ src/sfpowerkitCommand.ts
@@ -1,5 +1,5 @@
 import { SfdxCommand } from "@salesforce/command";
-import {COLOR_HEADER, LoggerLevel, SFPowerkit} from "./sfpowerkit";
+import SFPKLogger, {COLOR_HEADER, LoggerLevel} from "./SFPKLogger";
 
 /**
  * A base class that provides common funtionality for sfpowerscripts commands
@@ -14,15 +14,17 @@ export default abstract class SFPowerkitCommand extends SfdxCommand {
   /**
    * Command run code goes here
    */
-  abstract excute(): Promise<any>;
+  abstract execute(): Promise<any>;
 
   /**
    * Entry point for the commands
    */
   async run(): Promise<any> {
     // Always enable color by default
-    if (process.env.SFPOWERKIT_NOCOLOR) SFPowerkit.disableColor();
-    else SFPowerkit.enableColor();
+    if (process.env.SFPOWERKIT_NOCOLOR) SFPKLogger.disableColor();
+    else SFPKLogger.enableColor();
+
+    this.setLogLevel();
 
     for (const plugin of this.config.plugins) {
       if (plugin.name === "sfpowerkit") {
@@ -30,23 +32,40 @@ export default abstract class SFPowerkitCommand extends SfdxCommand {
         }
       }
 
-      SFPowerkit.log(
+
+    SFPKLogger.log(
       COLOR_HEADER(
         `-------------------------------------------------------------------------------------------`
-      ), LoggerLevel.INFO
+      )
     );
-    SFPowerkit.log(
+    SFPKLogger.log(
       COLOR_HEADER(
         `sfpowerkit  -- The DX@Scale Developer Toolkit -Version:${this.sfpowerkitConfig.version} -Release:${this.sfpowerkitConfig.pjson.release}`
-      ), LoggerLevel.INFO
+      )
     );
 
-    SFPowerkit.log(
+    SFPKLogger.log(
       COLOR_HEADER(
         `-------------------------------------------------------------------------------------------`
-      ), LoggerLevel.INFO
+      )
     );
 
-    await this.excute();
+    await this.execute();
   }
+
+  private setLogLevel() {
+    if (this.flags.loglevel === "trace" || this.flags.loglevel === "TRACE")
+      SFPKLogger.logLevel = LoggerLevel.TRACE;
+    else if (this.flags.loglevel === "debug" || this.flags.loglevel === "DEBUG")
+      SFPKLogger.logLevel = LoggerLevel.DEBUG;
+    else if (this.flags.loglevel === "info" || this.flags.loglevel === "INFO")
+      SFPKLogger.logLevel = LoggerLevel.INFO;
+    else if (this.flags.loglevel === "warn" || this.flags.loglevel === "WARN")
+      SFPKLogger.logLevel = LoggerLevel.WARN;
+    else if (this.flags.loglevel === "error" || this.flags.loglevel === "ERROR")
+      SFPKLogger.logLevel = LoggerLevel.ERROR;
+    else if (this.flags.loglevel === "fatal" || this.flags.loglevel === "FATAL")
+      SFPKLogger.logLevel = LoggerLevel.FATAL;
+    else SFPKLogger.logLevel = LoggerLevel.INFO;
+}
 }
