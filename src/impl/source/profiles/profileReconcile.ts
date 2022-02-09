8b2b0a8c18397ec1bcff5e78e1f4ad5952dbd0fc
azlam-abdulsalam <azlam.abdulsalam@accenture.com>
2022-02-10 08:18:05 +1100

    fix(reconcile): fix #632 Broken reconcile
    Reconcile command was recently added support for using workers. However bugs where introduced which
    made reconcile not working when used without linking plugins
    
    fix #632

--- src/impl/source/profiles/profileReconcile.ts
+++ src/impl/source/profiles/profileReconcile.ts
@@ -1,13 +1,13 @@
 import { LoggerLevel, SFPowerkit } from "../../../sfpowerkit";
-import MetadataFiles from "../../metadata/metadataFiles"; 
+import MetadataFiles from "../../metadata/metadataFiles";
 import * as path from "path";
 import { METADATA_INFO } from "../../metadata/metadataInfo";
 import * as _ from "lodash";
 import ProfileActions from "./profileActions";
 import FileUtils from "../../../utils/fileutils";
+import * as fs from "fs-extra";
 
-
-import { Worker } from 'worker_threads';
+import { Worker } from "worker_threads";
 
 export default class ProfileReconcile extends ProfileActions {
   metadataFiles: MetadataFiles;
@@ -49,61 +49,73 @@ export default class ProfileReconcile extends ProfileActions {
     );
 
     //Find Profiles to Reconcile
-    let profilesToReconcile:string[] = this.findProfilesToReconcile(profileList);
+    let profilesToReconcile: string[] = this.findProfilesToReconcile(
+      profileList
+    );
 
-   //Reconcile one first, then do the rest later to use cache for subsequent one
-   if(profilesToReconcile.length>1)
-   {
-     await this.runWorkers([profilesToReconcile[0]],destFolder);
-     profilesToReconcile.shift();
-   }
+    //Reconcile one first, then do the rest later to use cache for subsequent one
+    if (profilesToReconcile.length > 1) {
+      await this.runWorkers([profilesToReconcile[0]], destFolder);
+      profilesToReconcile.shift();
+    }
 
     return await this.runWorkers(profilesToReconcile, destFolder);
   }
 
-  private runWorkers(profilesToReconcile:string[], destFolder){
-    let workerCount=0;
-    let finishedWorkerCount =0;
-    let chunk=10; // One worker to process 10 profiles
-    let i:number;
-    let profileCount= profilesToReconcile.length;
+  private runWorkers(profilesToReconcile: string[], destFolder) {
+    let workerCount = 0;
+    let finishedWorkerCount = 0;
+    let chunk = 10; // One worker to process 10 profiles
+    let i: number;
+    let profileCount = profilesToReconcile.length;
     let result: string[] = [];
 
-    let workerPromise = new Promise<string[]>((resolve, reject)=>{
+    let workerPromise = new Promise<string[]>((resolve, reject) => {
       for (i = 0; i < profileCount; i += chunk) {
         workerCount++;
-        let temparray:string[] = profilesToReconcile.slice(i, i + chunk);
-        
-        SFPowerkit.log(`Initiated Profile reconcile thread :${workerCount}  with a chunk of ${temparray.length} profiles`,LoggerLevel.INFO);
-        SFPowerkit.log(`Profiles queued in thread :${workerCount} :`,LoggerLevel.INFO);
-        SFPowerkit.log(`${JSON.stringify(temparray)}`,LoggerLevel.INFO);
+        let temparray: string[] = profilesToReconcile.slice(i, i + chunk);
 
-        const worker = new Worker(path.resolve(__dirname, './worker.js') , {
+        SFPowerkit.log(
+          `Initiated Profile reconcile thread :${workerCount}  with a chunk of ${temparray.length} profiles`,
+          LoggerLevel.INFO
+        );
+        SFPowerkit.log(
+          `Profiles queued in thread :${workerCount} :`,
+          LoggerLevel.INFO
+        );
+        SFPowerkit.log(`${JSON.stringify(temparray)}`, LoggerLevel.INFO);
+        let reconcileWorkerFile;
+
+        //Switch to typescript while run locally using sfdx link, for debugging, else switch to js
+        if (fs.existsSync(path.resolve(__dirname, `./reconcileWorker.js`))) {
+          reconcileWorkerFile = `./reconcileWorker.js`;
+        } else {
+          reconcileWorkerFile = `./reconcileWorker.ts`;
+        }
+
+        const worker = new Worker(path.resolve(__dirname, "./worker.js"), {
           workerData: {
             profileChunk: temparray,
             destFolder: destFolder,
-            targetOrg:  this.org.getUsername(),
-            loglevel:  SFPowerkit.logLevelString,
-            isJsonFormatEnabled:  SFPowerkit.isJsonFormatEnabled,
-            path: './reconcileWorker.ts'
-          }
+            targetOrg: this.org.getUsername(),
+            loglevel: SFPowerkit.logLevelString,
+            isJsonFormatEnabled: SFPowerkit.isJsonFormatEnabled,
+            path: reconcileWorkerFile,
+          },
         });
-         
-        worker.on('message', (data) => {
+
+        worker.on("message", (data) => {
           // eslint-disable-next-line @typescript-eslint/no-array-constructor
-          let completedProfiles:string[]=new Array();
+          let completedProfiles: string[] = new Array();
           completedProfiles.push(...data);
           for (const profile of completedProfiles) {
-            SFPowerkit.log(
-              `Reconciled Profile ${profile}`,
-              LoggerLevel.INFO
-            );
+            SFPowerkit.log(`Reconciled Profile ${profile}`, LoggerLevel.INFO);
           }
           result.push(...data);
         });
 
-        worker.on('error', reject);
-        worker.on('exit', (code) => {
+        worker.on("error", reject);
+        worker.on("exit", (code) => {
           finishedWorkerCount++;
           if (code !== 0)
             //reject(new Error(`Worker stopped with exit code ${code}`));
@@ -112,8 +124,7 @@ export default class ProfileReconcile extends ProfileActions {
               LoggerLevel.ERROR
             );
 
-         
-          if(workerCount===finishedWorkerCount){
+          if (workerCount === finishedWorkerCount) {
             resolve(result);
           }
         });
@@ -122,7 +133,6 @@ export default class ProfileReconcile extends ProfileActions {
     return workerPromise;
   }
 
-  
   private findProfilesToReconcile(profileList: string[]) {
     let profilesToReconcile;
     if (profileList.length > 0) {
@@ -154,6 +164,4 @@ export default class ProfileReconcile extends ProfileActions {
       FileUtils.mkDirByPathSync(destFolder);
     }
   }
-
-  
 }
