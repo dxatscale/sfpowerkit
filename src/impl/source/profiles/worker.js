3e5f8b18cda13350f9be80a89aa770ceb01b92d7
Caitlyn Mills <63215232+Caitlyn-Mills@users.noreply.github.com>
2022-09-26 14:04:41 +0930

    feat(workers.js): remove workers.js for clarity

--- src/impl/source/profiles/worker.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const { workerData } = require('worker_threads');
-
-if (workerData.path.endsWith('.ts')) require('ts-node').register();
-
-require(path.resolve(__dirname, workerData.path));
